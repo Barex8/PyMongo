1afbc5d7ab02984f045dff5ac6cb345d75d38b38
Barex8 <alebalsas8@gmail.com>
2025-01-28 13:30:27 +0100

    Deletes, updates, y consultas

--- PyMongo.py
+++ PyMongo.py
@@ -18,24 +18,32 @@ collist = mydb.list_collection_names()
 if "Ejercicio" in collist:
   print("The collection exists.")
 
-  
+#Insertar  
 def insertarEjercicio(nombre,dificultad):
-    mydict = { "nombre": nombre, "dificultad": dificultad }
-    mycol.insert_one(mydict)
+  mydict = { "nombre": nombre, "dificultad": dificultad }
+  mycol.insert_one(mydict)
 
-
+#Update
 def updateNombreEjercicio(nombre,nuevoNombre):
-    myquery = { "nombre": nombre }
-    newvalues = { "$set": { "nombre": nuevoNombre } }
-    mycol.update_one(myquery, newvalues)
+  myquery = { "nombre": nombre }
+  newvalues = { "$set": { "nombre": nuevoNombre } }
+  mycol.update_one(myquery, newvalues)
 
 def updateDificultadEjercicio(nombre,dificultad):
-    myquery = { "nombre": nombre }
-    newvalues = { "$set": { "dificultad": dificultad} }
-    mycol.update_one(myquery, newvalues)
+  myquery = { "nombre": nombre }
+  newvalues = { "$set": { "dificultad": dificultad} }
+  mycol.update_one(myquery, newvalues)
 
+#Delete
+def deleteOneEjercicio(nombre):
+  myquery = { "nombre": nombre }
+  mycol.delete_one(myquery)
 
-
+#Mostrar consultas
+def MostrarConsultas(mydoc):
+  for x in mydoc:
+    print(x)
+  print()
 #Primera insercion de datos
 
 #insertarEjercicio("Sentadilla",1)
@@ -47,8 +55,8 @@ def updateDificultadEjercicio(nombre,dificultad):
 #insertarEjercicio("Remo de espalda baja",1)
 
 #Inserción Adicional
-insertarEjercicio("Extension de gemelos",3)
-insertarEjercicio("Bicicleta",0)
+#insertarEjercicio("Extension de gemelos",3)
+#insertarEjercicio("Bicicleta",0)
 
 #Tambien se puede hacer así
 #mylist = [
@@ -56,10 +64,29 @@ insertarEjercicio("Bicicleta",0)
 #  { "nombre": "Bicicleta", "dificultad":0}]
 #x = mycol.insert_many(mylist)
 
-updateNombreEjercicio("Bicicleta","Bicicleta Estatica")
+#Updates
+"""updateNombreEjercicio("Bicicleta","Bicicleta Estatica")
 updateNombreEjercicio("Plancha","Extensión de Tríceps")
-updateDificultadEjercicio("Bicicleta","Bicicleta Estatica")
-updateNombreEjercicio("Bicicleta","Bicicleta Estatica")
+updateDificultadEjercicio("Bicicleta Estatica",3)
+updateDificultadEjercicio("Plancha",3)
+updateNombreEjercicio("Remo de espalda baja","Remo de espalda alta")
+updateDificultadEjercicio("Sentadilla",1)
+updateDificultadEjercicio("Flexion",2)
+"""
+
+#Deletes
+""" deleteOneEjercicio("Plancha")
+deleteOneEjercicio("Sentadilla")
+deleteOneEjercicio("Flexion")
+ """
+
+#Consultas avanzadas
+MostrarConsultas(mycol.find({ "dificultad": { "$gt": 2 } })) #Muestra las dificultades mayores a 2
+MostrarConsultas(mycol.find({ "dificultad": { "$lt": 2 } })) #Muestra las dificultades menores a 2
+MostrarConsultas(mycol.find({ "dificultad": 2})) #Muestra las dificultades iguales a 2
+MostrarConsultas(mycol.find({ "dificultad" : 1 "&&" : 4})) #Muestra las dificultades mayores que 1 y menores que 4
+
+
 
 
 
